74dbc35c099fee40c7c95f2c12afafc5c2c35d4b
Cristiano <contato@cristianoprogramador.com>
2021-04-21 09:00:04 -0300

    Cria a listagem dos arquivos

--- api/models/commentModel.js
+++ api/models/commentModel.js
@@ -1,5 +1,9 @@
-const getAll = () => {
+const fs = require('fs');
+const filePath = `${__dirname}/../data/data.json`;
 
+const getAll = () => {
+    const fileText = fs.readFileSync(filePath);
+    return JSON.parse(fileText);
 }
 
 const insert = (comment) => {
