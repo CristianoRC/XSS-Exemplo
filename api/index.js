2e27b8d4c3b0d9b5cec85a3ee23191f79bd4424e
Cristiano <contato@cristianoprogramador.com>
2021-04-21 09:18:58 -0300

    Configura Cors na API

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const commentService = require('./services/commentsService');
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
