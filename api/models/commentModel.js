b1035cddf7b505b71a1c01d1971add12f43f4f29
Cristiano <contato@cristianoprogramador.com>
2021-04-21 09:02:00 -0300

    Implementa a inserçao dos cometarios

--- api/models/commentModel.js
+++ api/models/commentModel.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
-const filePath = `${__dirname}/../data/data.json`;
+const FILE_PATH = `${__dirname}/../data/data.json`;
 
 const getAll = () => {
-    const fileText = fs.readFileSync(filePath);
+    const fileText = fs.readFileSync(FILE_PATH);
     return JSON.parse(fileText);
 }
 
 const insert = (comment) => {
-
+    const allComments = getAll();
+    allComments.push(comment);
+    fs.writeFileSync(FILE_PATH, JSON.stringify(allComments));
 }
 
 module.exports = { getAll, insert };
